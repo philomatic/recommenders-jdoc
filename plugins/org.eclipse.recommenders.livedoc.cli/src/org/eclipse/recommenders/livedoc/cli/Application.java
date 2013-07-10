11a7d70c0cd9643e83646ed48fbbe33b4ee6e5e5
Patrick Gottschaemmer <mail@gottschaemmer.net>
2013-07-10 12:07:52 +0200

    fix javadoc package structure bug (e.g. for com.google.guava)

--- plugins/org.eclipse.recommenders.livedoc.cli/src/org/eclipse/recommenders/livedoc/cli/Application.java
+++ plugins/org.eclipse.recommenders.livedoc.cli/src/org/eclipse/recommenders/livedoc/cli/Application.java
@@ -1,9 +1,11 @@
 package org.eclipse.recommenders.livedoc.cli;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.net.URL;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.eclipse.core.runtime.NullProgressMonitor;
@@ -23,9 +25,6 @@ import org.slf4j.LoggerFactory;
 import org.sonatype.aether.artifact.Artifact;
 import org.sonatype.aether.util.artifact.DefaultArtifact;
 
-import com.google.common.collect.Lists;
-
-
 /**
  * This class controls all aspects of the application's execution
  */
@@ -36,25 +35,25 @@ public class Application implements IApplication {
     private static final File INDEX_DIR = new File(TEMP_DIR, "indexes");
     private Logger log = LoggerFactory.getLogger(getClass());
     private CLIOptions settings;
-    
-    
-    
-	/* (non-Javadoc)
-	 * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.IApplicationContext)
-	 */
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app. IApplicationContext)
+     */
     @SuppressWarnings({ "static-access", "unchecked" })
     public Object start(IApplicationContext context) throws Exception {
-        
+
         // Parsing arguments
-        
+
         Iterator<String[]> iter = context.getArguments().values().iterator();
         String[] args = null;
-        if (iter.hasNext()){
+        if (iter.hasNext()) {
             args = iter.next();
-        } else{
+        } else {
             System.err.println("No Arguments detected");
         }
-        
+
         settings = new CLIOptions();
         CmdLineParser parser = new CmdLineParser(settings);
         parser.registerHandler(URL.class, ExtURLOptionHandler.class);
@@ -67,80 +66,89 @@ public class Application implements IApplication {
             parser.printUsage(System.err);
             this.stop();
         }
-        
+
         CACHE_DIR.mkdirs();
         INDEX_DIR.mkdirs();
 
         IRepositoryBroker repoBroker = RepoBrokerProvider.create(CACHE_DIR, INDEX_DIR);
-        RepositoryDescriptor sourceRepositoryDescriptor = new RepositoryDescriptor("sourceRepo", settings.getSourceRepo());
-        
+        RepositoryDescriptor sourceRepositoryDescriptor = new RepositoryDescriptor("sourceRepo",
+                settings.getSourceRepo());
+
         repoBroker.ensureIndexUpToDate(sourceRepositoryDescriptor, new NullProgressMonitor());
-        
+
         String coordinates = settings.getMavenCoordinates();
-        
+
         StringBuffer sb = new StringBuffer(coordinates);
         int lastColon = coordinates.lastIndexOf(":");
         sb.insert(lastColon, ":jar:sources");
-        
+
         Artifact artifact = new DefaultArtifact(sb.toString());
         artifact = repoBroker.download(artifact, sourceRepositoryDescriptor, new NullProgressMonitor());
 
-        File sourceFiles = ZipUtils.unzipToSeparateFolder(artifact.getFile(), new File(TEMP_DIR)); 
-        
-        //TODO: ugly...
-        ArrayList<String> subpackages = Lists.newArrayListWithCapacity(1);
-        subpackages.add(artifact.getGroupId());
-        
+        File sourceFiles = ZipUtils.unzipToSeparateFolder(artifact.getFile(), new File(TEMP_DIR));
+
+        List<String> subpackages = Arrays.asList(sourceFiles.list(new FilenameFilter() {
+
+            @Override
+            public boolean accept(File dir, String name) {
+                return !name.equals("META-INF");
+            }
+        }));
+
         File livedocDir = new File(TEMP_DIR + File.separator + "livedoc_tmp");
-        
-        ILiveDoc livedoc = new LiveDoc(settings.isVerbose(), sourceFiles, livedocDir, subpackages);
-        livedoc.generate();
-        
+
         sb.delete(0, sb.length());
         sb.append(artifact.getArtifactId());
         sb.append("-");
         sb.append(artifact.getVersion());
         sb.append("-javadoc");
-        
-        if (settings.getOutputDir() != null){
+
+        File tmpOutput = new File(livedocDir, sb.toString());
+
+        ILiveDoc livedoc = new LiveDoc(settings.isVerbose(), sourceFiles, tmpOutput, subpackages);
+        livedoc.generate();
+
+        if (settings.getOutputDir() != null) {
             File output = new File(settings.getOutputDir().getAbsolutePath() + File.separator + sb.toString());
-            FileUtils.copyDirectory(livedocDir, output);
+            FileUtils.copyDirectory(tmpOutput, output);
         }
-     
+
         sb.append(".jar");
-        
-        
-        File tmpOutput = new File(TEMP_DIR + File.separator + sb.toString());
-        ZipUtils.zip(livedocDir, tmpOutput);
-        
+
+        File tmpJar = new File(livedocDir, sb.toString());
+        ZipUtils.zip(tmpOutput, tmpJar);
+
         if (settings.isJarOutput()) {
             File output = new File(settings.getOutputDir().getAbsolutePath() + File.separator + sb.toString());
-            FileUtils.copyFile(tmpOutput, output);
+            FileUtils.copyFile(tmpJar, output);
         }
-        
-        if (settings.getUploadRepo() != null){
-            Artifact uploadArtifact = new DefaultArtifact(artifact.getGroupId(), artifact.getArtifactId(),"javadoc", "jar", artifact.getVersion());
+
+        if (settings.getUploadRepo() != null) {
+            Artifact uploadArtifact = new DefaultArtifact(artifact.getGroupId(), artifact.getArtifactId(), "javadoc",
+                    "jar", artifact.getVersion());
             uploadArtifact = uploadArtifact.setFile(tmpOutput);
-            RepositoryDescriptor uploadRepositoryDescriptor = new RepositoryDescriptor("uploadRepo", settings.getUploadRepo());
+            RepositoryDescriptor uploadRepositoryDescriptor = new RepositoryDescriptor("uploadRepo",
+                    settings.getUploadRepo());
             repoBroker.upload(uploadArtifact, uploadRepositoryDescriptor, new NullProgressMonitor());
         }
-        
+
         System.out.println("Done.");
-        
+
         return IApplication.EXIT_OK;
     }
 
-
-    /* (non-Javadoc)
-	 * @see org.eclipse.equinox.app.IApplication#stop()
-	 */
-	public void stop() {
-	    try {
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.equinox.app.IApplication#stop()
+     */
+    public void stop() {
+        try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-		System.exit(0);
-	}
+        System.exit(0);
+    }
 }
